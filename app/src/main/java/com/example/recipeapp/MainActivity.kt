f08e926567063bda2e3ae3f02ba78198541f2557
Chayanee <s210973@dtu.dk>
2023-10-22 01:54:32 +0200

    Added top bar to main page

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.databinding.DataBindingUtil
+import com.example.recipeapp.databinding.ActivityMainBinding
 import com.example.recipeapp.ui.theme.RecipeAppTheme
 
 class MainActivity : ComponentActivity() {
+    //private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //binding =DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
